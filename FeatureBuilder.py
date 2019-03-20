6df6dc8682596238d6c3b083202aa88e3d4e6000
yunhaoli <yunhaoli.cs@gmail.com>
2019-03-19 21:25:58 -0400

    Finished java compile calling in py.

--- FeatureBuilder.py
+++ FeatureBuilder.py
@@ -1,14 +1,54 @@
+import os
+
 class FeatureBuilder:
     def __init__(self, input_path = "./CONLL_train.pos-chunk-name"):
         out_path = input_path
-        out_path = out_path[out_path.rfind("/")+1 : out_path.rfind(".") + ".name"]
+        out_path = out_path[out_path.rfind("/")+1 : out_path.rfind(".")] + ".name"
+        print("Output:", out_path)
+        self.in_file = open(input_path, 'r')
+        self.out_file = open(out_path, 'w')
 
-    def exec_line(self, line):
-        pass
+    @staticmethod
+    def exec_line(line):
+        for i in range(0, 32):
+            line = line.replace(chr(i), " ")
+        line = line.strip().split(" ")
+        return tuple(i for i in line)
 
     def append_feature(self, feature):
         pass
 
     def close_file(self):
-        pass
+        self.in_file.close()
+        self.out_file.close()
 
+    def run(self):
+        sentence = []
+        count = 0
+        while True:
+            line = self.in_file.readline()
+            if not line:
+                break
+            data = self.exec_line(line)
+            if data[0] == "-DOCSTART-":
+                # skip the first 2 lines
+                self.in_file.readline()
+                continue
+            sentence.append(data)
+            if len(data) == 1:
+                count += 1
+                # print(count, "->sentence = ", sentence)
+                sentence = []
+
+        print("Finished.")
+        self.close_file()
+
+        print("There is ", count, "sentences in training file.")
+
+
+if __name__ == '__main__':
+    builder = FeatureBuilder()
+    builder.run()
+
+    os.system("rm *.class")
+    os.system("javac -cp ./maxent-3.0.0.jar:trove.jar ./*.java")
