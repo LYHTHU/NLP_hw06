e6d7f963164a9fdf54449f5ed04760aa634a6d0c
yunhaoli <yunhaoli.cs@gmail.com>
2019-03-20 15:50:26 -0400

    Need to figure out how to deal with "Docstart" label.

--- FeatureBuilder.py
+++ FeatureBuilder.py
@@ -5,6 +5,7 @@ class FeatureBuilder:
         self.out_path = input_path[input_path.rfind("/")+1: input_path.rfind(".")] + ".feature"
         self.in_file = open(input_path, 'r')
         self.out_file = open(self.out_path, 'w')
+        self.train_mode = train_mode
 
     @staticmethod
     def exec_line(line):
@@ -13,18 +14,13 @@ class FeatureBuilder:
         line = line.strip().split(" ")
         return tuple(i for i in line)
 
-    def append_feature(self, token, feature):
-        str = token
-        for f in feature:
-            str = str + "\t" + "feature=" + f
-        str = str + "\n"
-        self.out_file.write(str)
-
     def append_feature(self, token, feature, tag):
         str = token
         for f in feature:
             str = str + "\t" + "feature=" + f
-        str = str + "\t" + tag + "\n"
+        if self.train_mode:
+            str = str + "\t" + tag
+        str = str + "\n"
         self.out_file.write(str)
 
     def close_file(self):
@@ -32,15 +28,27 @@ class FeatureBuilder:
         self.out_file.close()
 
     def exec_sentence(self, sentence):
+        length = len(sentence)
         for i, data in enumerate(sentence):
-            token, pos, bio, tag = data[0], data[1], data[2], data[3]
+            if self.train_mode:
+                token, pos, bio, tag = data[0], data[1], data[2], data[3]
+            else:
+                token, pos, bio = data[0], data[1], data[2]
+
             if i > 0:
                 pre = sentence[i - 1]
-                feature = (pos, pre[1])
+            else:
+                pre = (None, "start", 0, 0)
+            if i < length - 1:
+                post = sentence[i + 1]
+            else:
+                post = pre = (None, "end", 0, 0)
+            feature = (pos, pre[1], post[1])
+
+            if self.train_mode:
                 self.append_feature(token, feature, tag)
             else:
-                feature = (pos, "start")
-                self.append_feature(token, feature, tag)
+                self.append_feature(token, feature, None)
 
     def run(self):
         sentence = []
@@ -77,7 +85,12 @@ if __name__ == '__main__':
 
     model_name = "MEmodel.bin.gz"
     dev_name = "CONLL_dev.pos-chunk"
+    dev_feature = "CONLL_dev.feature"
     dev_out = "response.name"
     os.system("java -cp .:./maxent-3.0.0.jar:trove.jar MEtrain " + builder.out_path + " " + model_name)
-    os.system("java -cp .:./maxent-3.0.0.jar:trove.jar MEtag " + dev_name + " " + model_name + " " + dev_out)
+
+    builder = FeatureBuilder(input_path=dev_name, train_mode=False)
+    builder.run()
+
+    os.system("java -cp .:./maxent-3.0.0.jar:trove.jar MEtag " + dev_feature + " " + model_name + " " + dev_out)
     os.system("python3 score.name.py")
